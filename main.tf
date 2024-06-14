befbadbe665963e25a4d57e1c40fd9dc0665f286
Mike Wies <mike.wies@snowflake.com>
2024-06-14 18:16:47 +0000

    some change

--- main.tf
+++ main.tf
@@ -22,3 +22,10 @@ resource "snowflake_database" "demo_db_mike_wies" {
   name    = "DEMO_DB_MIKE_WIES"
   comment = "Database for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db_mike_wies.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
+
